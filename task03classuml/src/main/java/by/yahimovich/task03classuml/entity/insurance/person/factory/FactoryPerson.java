4f25277a522d42d6d6cf9d2d2b9c1d8433e1f0be
Yahimchik <egor.yaximovich@gmail.com>
2021-12-24 00:35:32 +0300

    add task 03

--- task03classuml/src/main/java/by/yahimovich/task03classuml/entity/insurance/person/factory/FactoryPerson.java
+++
@@ -1,7 +0,0 @@
-package by.yahimovich.task03classuml.entity.insurance.person.factory;
-
-import by.yahimovich.task03classuml.entity.insurance.person.Person;
-
-public interface FactoryPerson {
-    Person createPerson();
-}
